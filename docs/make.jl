472f204f2d980781f78cefadbcc2ddea14c20e2e
John D. Ramsdell <ramsdell@mitre.org>
2023-11-03 13:36:16 -0400

    Make docs on GitHub

--- docs/make.jl
+++ docs/make.jl
@@ -5,16 +5,11 @@
 # julia --project make.jl
 
 using Documenter, ElectionGuardVerifier1X
-import Documenter.Remotes.GitLab  # Temporary
 
 makedocs(
     sitename = "ElectionGuard Verifier",
     authors = "John D. Ramsdell and Moses D. Liskov",
     format = Documenter.HTML(prettyurls = false),
-    # repo definition is temporary until the move to GitHub
-    repo = GitLab("gitlab.mitre.org",
-                  "electionguard",
-                  "mitre-electionguard-verifier-1x"),
 
     pages = [
         "Home" => "index.md",
